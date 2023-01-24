99c8673db736342af24a3a8eb4886ac9fa212210
Dousir9 <736191200@qq.com>
2023-01-24 23:53:32 +0800

    finish txn basic

--- src/client.rs
+++ src/client.rs
@@ -2,8 +2,9 @@ use futures::executor::block_on;
 use labrpc::Error;
 use labrpc::*;
 
-use crate::msg::{self, TimestampRequest};
+use crate::msg::{self, CommitRequest, TimestampRequest};
 use crate::service::{TSOClient, TransactionClient};
+use std::collections::HashMap;
 use std::{thread, time};
 
 // BACKOFF_TIME_MS is the wait time before retrying to send the request.
@@ -25,6 +26,8 @@ pub struct Client {
     // Your definitions here.
     tso_client: TSOClient,
     txn_client: TransactionClient,
+    start_ts: u64,
+    mem_buffer: HashMap<Vec<u8>, Vec<u8>>,
 }
 
 impl Client {
@@ -34,6 +37,8 @@ impl Client {
         Client {
             tso_client,
             txn_client,
+            start_ts: 0,
+            mem_buffer: HashMap::new(),
         }
     }
 
@@ -46,12 +51,15 @@ impl Client {
                     .get_timestamp(&msg::TimestampRequest {})
                     .await
             }) {
-                Ok(response) => return Ok(0),
+                Ok(response) => return Ok(response.timestamp),
                 Err(error) => {
                     warn!("get_timestamp resquest failed because {}", error);
-                    thread::sleep(time::Duration::from_millis(
-                        ((1 << i) * BACKOFF_TIME_MS) as u64,
-                    ));
+                    match error {
+                        Error::Timeout => thread::sleep(time::Duration::from_millis(
+                            ((1 << i) * BACKOFF_TIME_MS) as u64,
+                        )),
+                        other => return Err(other),
+                    }
                 }
             }
         }
@@ -60,25 +68,141 @@ impl Client {
 
     /// Begins a new transaction.
     pub fn begin(&mut self) {
-        // Your code here.
-        unimplemented!()
+        self.start_ts = self
+            .get_timestamp()
+            .expect("Unexpected error occurs when get_timestamp");
+        info!("begin with start_ts {}", self.start_ts);
     }
 
     /// Gets the value for a given key.
     pub fn get(&self, key: Vec<u8>) -> Result<Vec<u8>> {
         // Your code here.
-        unimplemented!()
+        let get_request = msg::GetRequest {
+            key,
+            start_ts: self.start_ts,
+        };
+        for i in 0..RETRY_TIMES {
+            match block_on(async { self.txn_client.get(&get_request).await }) {
+                Ok(reponse) => return Ok(reponse.value),
+                Err(error) => {
+                    warn!("get resquest failed because {}", error);
+                    match error {
+                        Error::Timeout => thread::sleep(time::Duration::from_millis(
+                            ((1 << i) * BACKOFF_TIME_MS) as u64,
+                        )),
+                        other => return Err(other),
+                    }
+                }
+            }
+        }
+        Err(Error::Timeout)
     }
 
     /// Sets keys in a buffer until commit time.
     pub fn set(&mut self, key: Vec<u8>, value: Vec<u8>) {
         // Your code here.
-        unimplemented!()
+        self.mem_buffer.insert(key, value);
+    }
+
+    fn inner_prewrite(
+        &self,
+        start_ts: u64,
+        key: Vec<u8>,
+        value: Vec<u8>,
+        primary: Vec<u8>,
+    ) -> Result<bool> {
+        let prewrite_request = msg::PrewriteRequest {
+            start_ts,
+            key,
+            value,
+            primary,
+        };
+        for i in 0..RETRY_TIMES {
+            match block_on(async { self.txn_client.prewrite(&prewrite_request).await }) {
+                Ok(response) => return Ok(response.success),
+                Err(error) => match error {
+                    Error::Timeout => thread::sleep(time::Duration::from_millis(
+                        ((1 << i) * BACKOFF_TIME_MS) as u64,
+                    )),
+                    other => return Err(other),
+                },
+            }
+        }
+        Err(Error::Timeout)
+    }
+
+    fn inner_commit(
+        &self,
+        start_ts: u64,
+        commit_ts: u64,
+        key: Vec<u8>,
+        is_primary: bool,
+    ) -> Result<bool> {
+        let commit_request = CommitRequest {
+            start_ts,
+            commit_ts,
+            key,
+            is_primary,
+        };
+        for i in 0..RETRY_TIMES {
+            match block_on(async { self.txn_client.commit(&commit_request).await }) {
+                Ok(response) => {
+                    return Ok(response.success);
+                }
+                Err(error) => match error {
+                    Error::Timeout => thread::sleep(time::Duration::from_millis(
+                        ((1 << i) * BACKOFF_TIME_MS) as u64,
+                    )),
+                    other => return Err(other),
+                },
+            }
+        }
+        Err(Error::Timeout)
     }
 
     /// Commits a transaction.
     pub fn commit(&self) -> Result<bool> {
         // Your code here.
-        unimplemented!()
+        if self.mem_buffer.is_empty() {
+            return Ok(true);
+        }
+
+        let start_ts = self.start_ts;
+        let mutations: Vec<(&Vec<u8>, &Vec<u8>)> = self.mem_buffer.iter().collect();
+        let primary = &mutations[0];
+        let secondaries = &mutations[1..];
+
+        if !self.inner_prewrite(
+            start_ts,
+            primary.0.to_vec(),
+            primary.1.to_vec(),
+            primary.0.to_vec(),
+        )? {
+            return Ok(false);
+        }
+
+        for secondary in secondaries {
+            if !self.inner_prewrite(
+                start_ts,
+                secondary.0.to_vec(),
+                secondary.1.to_vec(),
+                primary.0.to_vec(),
+            )? {
+                return Ok(false);
+            }
+        }
+
+        let commit_ts = self.get_timestamp()?;
+        if !self.inner_commit(start_ts, commit_ts, primary.0.to_vec(), true)? {
+            return Ok(false);
+        }
+
+        for secondary in secondaries {
+            if !self.inner_commit(start_ts, commit_ts, secondary.0.to_vec(), false)? {
+                return Ok(false);
+            }
+        }
+
+        Ok(true)
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,6 @@
 use std::collections::BTreeMap;
+use std::ops::Bound::Included;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
@@ -14,6 +16,7 @@ const TTL: u64 = Duration::from_millis(100).as_nanos() as u64;
 #[derive(Clone, Default)]
 pub struct TimestampOracle {
     // You definitions here if needed.
+    timestamp_gemerator: Arc<AtomicU64>,
 }
 
 #[async_trait::async_trait]
@@ -21,14 +24,16 @@ impl timestamp::Service for TimestampOracle {
     // example get_timestamp RPC handler.
     async fn get_timestamp(&self, _: TimestampRequest) -> labrpc::Result<TimestampResponse> {
         // Your code here.
-        Ok(TimestampResponse {})
+        Ok(TimestampResponse {
+            timestamp: self.timestamp_gemerator.fetch_add(1, Ordering::Relaxed),
+        })
     }
 }
 
 // Key is a tuple (raw key, timestamp).
 pub type Key = (Vec<u8>, u64);
 
-#[derive(Clone, PartialEq)]
+#[derive(Clone, PartialEq, Debug)]
 pub enum Value {
     Timestamp(u64),
     Vector(Vec<u8>),
@@ -45,11 +50,11 @@ pub enum Column {
 
 // KvTable is used to simulate Google's Bigtable.
 // It provides three columns: Write, Data, and Lock.
-#[derive(Clone, Default)]
+#[derive(Clone, Default, Debug)]
 pub struct KvTable {
-    write: BTreeMap<Key, Value>,
-    data: BTreeMap<Key, Value>,
-    lock: BTreeMap<Key, Value>,
+    write: BTreeMap<Key, Value>, // {key, commit_ts} => {start_ts}
+    data: BTreeMap<Key, Value>,  // {key, start_ts} => {value}
+    lock: BTreeMap<Key, Value>,  // {key, start_ts} => {primary}
 }
 
 impl KvTable {
@@ -64,21 +69,42 @@ impl KvTable {
         ts_end_inclusive: Option<u64>,
     ) -> Option<(&Key, &Value)> {
         // Your code here.
-        unimplemented!()
+        let ts_start_inclusive = ts_start_inclusive.unwrap_or(0);
+        let ts_end_inclusive = ts_end_inclusive.unwrap_or(u64::MAX);
+        let map = match column {
+            Column::Write => &self.write,
+            Column::Data => &self.data,
+            Column::Lock => &self.lock,
+        };
+        map.range((
+            Included((key.clone(), ts_start_inclusive)),
+            Included((key, ts_end_inclusive)),
+        ))
+        .last()
     }
 
     // Writes a record to a specified column in MemoryStorage.
     #[inline]
     fn write(&mut self, key: Vec<u8>, column: Column, ts: u64, value: Value) {
         // Your code here.
-        unimplemented!()
+        let mut map = match column {
+            Column::Write => &mut self.write,
+            Column::Data => &mut self.data,
+            Column::Lock => &mut self.lock,
+        };
+        map.insert((key, ts), value);
     }
 
     #[inline]
     // Erases a record from a specified column in MemoryStorage.
     fn erase(&mut self, key: Vec<u8>, column: Column, commit_ts: u64) {
         // Your code here.
-        unimplemented!()
+        let mut map = match column {
+            Column::Write => &mut self.write,
+            Column::Data => &mut self.data,
+            Column::Lock => &mut self.lock,
+        };
+        map.remove(&(key, commit_ts));
     }
 }
 
@@ -94,19 +120,114 @@ impl transaction::Service for MemoryStorage {
     // example get RPC handler.
     async fn get(&self, req: GetRequest) -> labrpc::Result<GetResponse> {
         // Your code here.
-        unimplemented!()
+        loop {
+            let kvtable = self.data.lock().unwrap();
+            // 检查 [0, start_ts] 是否有有锁
+            if kvtable
+                .read(req.key.to_vec(), Column::Lock, Some(0), Some(req.start_ts))
+                .is_some()
+            {
+                continue;
+            }
+            // 通过 write 列找到 start_ts 可以看到的 key 的最新提交版本
+            match kvtable.read(req.key.to_vec(), Column::Write, None, Some(req.start_ts)) {
+                Some(write) => {
+                    // 根据版本读取数据
+                    if let Value::Timestamp(data_ts) = write.1 {
+                        if let Value::Vector(value) = kvtable
+                            .read(
+                                req.key.to_vec(),
+                                Column::Data,
+                                Some(*data_ts),
+                                Some(data_ts.clone()),
+                            )
+                            .unwrap()
+                            .1
+                        {
+                            return Ok(GetResponse {
+                                value: value.to_vec(),
+                            });
+                        }
+                    }
+                }
+                None => return Ok(GetResponse { value: vec![] }),
+            }
+        }
     }
 
     // example prewrite RPC handler.
     async fn prewrite(&self, req: PrewriteRequest) -> labrpc::Result<PrewriteResponse> {
         // Your code here.
-        unimplemented!()
+        // 检查 write-write 冲突
+        let mut kvtable = self.data.lock().unwrap();
+        if kvtable
+            .read(req.key.to_vec(), Column::Write, Some(req.start_ts), None)
+            .is_some()
+        {
+            return Ok(PrewriteResponse { success: false });
+        }
+
+        // 检查 key 是否被加锁
+        if kvtable
+            .read(req.key.to_vec(), Column::Lock, None, None)
+            .is_some()
+        {
+            return Ok(PrewriteResponse { success: false });
+        }
+
+        // 为 key 加锁
+        kvtable.write(
+            req.key.to_vec(),
+            Column::Lock,
+            req.start_ts,
+            Value::Vector(req.primary),
+        );
+
+        // 写 data 列
+        kvtable.write(
+            req.key,
+            Column::Data,
+            req.start_ts,
+            Value::Vector(req.value),
+        );
+
+        Ok(PrewriteResponse { success: true })
     }
 
     // example commit RPC handler.
     async fn commit(&self, req: CommitRequest) -> labrpc::Result<CommitResponse> {
         // Your code here.
-        unimplemented!()
+        let mut kvtable = self.data.lock().unwrap();
+        // 检查 lock 的合法性
+        match kvtable.read(req.key.to_vec(), Column::Lock, None, None) {
+            Some(lock) => {
+                let key_and_start_ts = lock.0;
+                if key_and_start_ts.1 != req.start_ts {
+                    return Ok(CommitResponse { success: false });
+                }
+                if req.is_primary {
+                    if let Value::Vector(primary) = lock.1 {
+                        if primary != &req.key {
+                            return Ok(CommitResponse { success: false });
+                        }
+                    }
+                }
+            }
+            None => return Ok(CommitResponse { success: false }),
+        }
+
+        // 写 write 列
+        kvtable.write(
+            req.key.to_vec(),
+            Column::Write,
+            req.commit_ts,
+            Value::Timestamp(req.start_ts),
+        );
+
+        // 删除 key 的 lock
+        kvtable.erase(req.key, Column::Lock, req.start_ts);
+
+        Ok(CommitResponse { success: true })
     }
 }
 
